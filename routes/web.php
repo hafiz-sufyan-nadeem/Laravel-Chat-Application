880c7069db1b24bd0eacfb416c2ba8dc155e2d2a
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-01-01 02:26:54 +0500

    Add chat route for chatpage view and send-message route for send messgae

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/chat', [MessageController::class, 'index'])->middleware('auth');
+
+Route::post('/send-message', [MessageController::class, 'store'])->middleware('auth');
 
 require __DIR__.'/auth.php';
