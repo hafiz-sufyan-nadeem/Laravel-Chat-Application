b45664a5c5a964b534820a4cbc09a7f659cadca9
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2026-01-02 02:02:39 +0500

    Add index and Store method in MessageController for view and store msgs

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -1,10 +1,27 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function index()
+    {
+        $messages = Message::with('user')->latest()->limit(50)->get();
+        return view('chat', compact('messages'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'message' => 'required',
+        ]);
+
+        Message::create([
+            'user_id' => auth()->id(),
+            'message' => $request->message
+        ]);
+        return back();
+    }
 }
